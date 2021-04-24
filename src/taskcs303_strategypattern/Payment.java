afad56f672f64c828faf888ec682f774f11800bd
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2021-04-24 02:16:19 +0200

    task

---
+++ src/taskcs303_strategypattern/Payment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taskcs303_strategypattern;
+
+/**
+ *
+ * @author Esraa
+ */
+public class Payment {
+    float amount ;
+    lauthorizeBehavior authorizeBehavior ;
+    lprintBehavior printBehavior ;
+}
